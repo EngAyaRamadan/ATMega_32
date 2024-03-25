3554f9c9b89c587385b0949dbf6b3b53e6c8cbbf
Aya-Ramadan <ayaramadan@gmail.com>
2024-03-25 21:38:14 +0200

    Bit Math Funcions

--- Driver/03-LIB/BIT_MATH.h
+++ Driver/03-LIB/BIT_MATH.h
@@ -11,7 +11,5 @@
 #define SET_BIT(reg,bit)  reg|=(1<<bit)
 #define CLR_BIT(reg,bit)  reg&=~(1<<bit)
 #define TOG_BIT(reg,bit)  reg^=(1<<bit)
-#define GET_BIT(reg,bit)  (reg&=(1<<bit))>>bit
-
-
+#define GET_BIT(reg,bit)  ((reg>>bit)&1)
 #endif
