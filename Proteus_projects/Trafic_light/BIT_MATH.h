d1069b36b3e9d143f64f5ac36701f0b09246bc18
Aya-Ramadan <ayaramadan@gmail.com>
2024-03-25 22:09:34 +0200

    Trafic Light project

--- Proteus_projects/Trafic_light/BIT_MATH.h
+++ Proteus_projects/Trafic_light/BIT_MATH.h
@@ -11,7 +11,7 @@
 #define SET_BIT(reg,bit)  reg|=(1<<bit)
 #define CLR_BIT(reg,bit)  reg&=~(1<<bit)
 #define TOG_BIT(reg,bit)  reg^=(1<<bit)
-#define GET_BIT(reg,bit)  (reg&=(1<<bit))>>bit
+#define GET_BIT(reg,bit)  ((reg>>bit)&1)
 
 
 #endif
